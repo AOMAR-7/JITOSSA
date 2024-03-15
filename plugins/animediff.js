85355574a44a9edc7a1e3a3acbcf76489426fbd1
OVMAR-DEV <163443219+OVMAR-DEV@users.noreply.github.com>
2024-03-15 10:40:51 +0000

    Update animediff.js

--- plugins/animediff.js
+++ plugins/animediff.js
@@ -1,15 +1,25 @@
-import gplay from 'google-play-scraper'
+import fetch from "node-fetch"
 
-let handler = async (m, { conn, text }) => {
-	if (!text) throw 'Input Query'
-	let res = await gplay.search({ term: text })
-	if (!res.length) throw `Query "${text}" not found :/`
-	let opt = { contextInfo: { externalAdReply: { title: res[0].title, body: res[0].summary, thumbnail: (await conn.getFile(res[0].icon)).data, sourceUrl: res[0].url }}}
-	res = res.map((v) => `*Title:* ${v.title}\n*Dev:* ${v.developer}\n*Price:* ${v.priceText}\n*Score:* ${v.scoreText}\n*Link:* ${v.url}`).join`\n\n`
-	m.reply(res, null, opt)
-}
-handler.help = ['apksearch']
-handler.tags = ['tools']
-handler.command = /^(apksearch)$/i
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+         let wm = global.me
+
+        if (!text) throw `This command generates image from texts\n\n Example usage\n${ usedPrefix + command } 1girl, blush, megane, school uniform`
+        await m.reply('*Processing image*')
+        await conn.relayMessage(m.chat, { reactionMessage: { key: m.key, text: '⌛'  }}, { messageId: m.key.id })
+        try {
+        let url = `https://api.xyroinee.xyz/api/ai/animediffusion?q=${text}&apikey=${global.xyro}`
+        await conn.sendFile(m.chat, await(await fetch(url)).buffer(), 'image.jpg', wm, m)
+        m.react(done)
+      } catch (e) {
+        console.log(e)
+        m.reply(eror)
+      }
+
+    }
+
+
+handler.help = ['animediff <text>']
+handler.tags = ['ai']
+handler.command = /^(animediff)$/i
 
 export default handler
