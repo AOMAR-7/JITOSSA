83f089ffaba94a87eede71b656b6607aa8e1da0a
OVMAR-DEV <163443219+OVMAR-DEV@users.noreply.github.com>
2024-03-15 05:47:06 +0000

    Delete plugins directory
    Signed-off-by: OVMAR-DEV <163443219+OVMAR-DEV@users.noreply.github.com>

--- plugins/_cek.js
+++
@@ -1,7 +0,0 @@
-let handler = async ( m ) => {
-  m.reply('تابع صانع البوت فى إنستجرام \n https://www.instagram.com/ovmar_1!')
-}
-
-handler.command = /^(cek|tes|alive|p|a)$/i
-
-export default handler
