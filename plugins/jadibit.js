8f11e0e8aee2c5520d1e3a8094bfe84d98378da4
OVMAR-DEV <163443219+OVMAR-DEV@users.noreply.github.com>
2024-03-15 08:47:09 +0000

    Update jadibit.js

--- plugins/jadibit.js
+++ plugins/jadibit.js
@@ -1,83 +1,84 @@
-let { MessageType } = require('@adiwajshing/baileys')
-const { is } = require('cheerio/lib/api/traversing')
-let qrcode = require('qrcode')
-let fs = require('fs')
-let path = require('path')
+import db from '../../lib/database.js'
 
-if (global.conns instanceof Array) console.log()// for (let i of global.conns) global.conns[i] && global.conns[i].user ? global.conns[i].close().then(() => delete global.conns[id] && global.conns.splice(i, 1)).catch(global.conn.logger.error) : delete global.conns[i] && global.conns.splice(i, 1)
-else global.conns = []
+let handler = async (m, { conn, command }) => {
+	let txt = db.data.datas.teksjadibot || `❤‍🩹 *[ Chat Dengan Creator ]*
+wa.me/6282337245566
 
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-  let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
- 
-  let parent = args[0] && args[0] == 'plz' ? conn : global.conn
-  let auth = false
-  if ((args[0] && args[0] == 'plz') || global.conn.user.jid == conn.user.jid) {
-    let id = global.conns.length
-    let conn = new global.conn.constructor()
-    if (args[0] && args[0].length > 200) {
-      let json = Buffer.from(args[0], 'base64').toString('utf-8')
-      // global.conn.reply(m.isGroup ? m.sender : m.chat, json, m)
-      let obj = JSON.parse(json)
-      await conn.loadAuthInfo(obj)
-      auth = true
-    }
-    conn.on('qr', async qr => {
-      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan this QR to be a temporary bot\n\n1. Click the three dots in the upper right corner\n2. Tap WhatsApp Web\n3. Scan this QR \nQR Expires in 20 seconds', m)
-      setTimeout(() => {
-        parent.deleteMessage(m.chat, scan.key)
-      }, 30000)
-    })
-    conn.welcome = global.conn.welcome + ''
-    conn.bye = global.conn.bye + ''
-    conn.spromote = global.conn.spromote + ''
-    conn.sdemote = global.conn.sdemote + ''
-    conn.handler = global.conn.handler.bind(conn)
-    conn.onDelete = global.conn.onDelete.bind(conn)
-    conn.onParticipantsUpdate = global.conn.onParticipantsUpdate.bind(conn)
-    conn.onGroupUpdate = global.conn.onGroupUpdate.bind(conn)
-    conn.onCall = global.conn.onCall.bind(conn)
-    conn.on('chat-update', conn.handler)
-    conn.on('message-delete', conn.onDelete)
-    conn.on('group-participants-update', conn.onParticipantsUpdate)
-    conn.on('group-update', conn.onGroupUpdate)
-    conn.on('CB:action,,call', conn.onCall)
-    conn.regenerateQRIntervalMs = null
-    conn.connect().then(async ({ user }) => {
-      parent.reply(m.chat, 'Successfully connected with your WhatsApp.\n*NOTE: This is just a ride*\n' + JSON.stringify(user, null, 2), m)
-      if (auth) return
-      await parent.sendMessage(user.jid, `You can login without qr with the message below. to get the complete code, please send *${usedPrefix}getcode* to get the accurate code`, MessageType.extendedText)
-      parent.sendMessage(user.jid, `${usedPrefix + command} ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, MessageType.extendedText)
-    })
-    setTimeout(() => {
-      if (conn.user) return
-      conn.close()
-      let i = global.conns.indexOf(conn)
-      if (i < 0) return
-      delete global.conns[i]
-      global.conns.splice(i, 1)
-    }, 60000)
-    conn.on('close', () => {
-      setTimeout(async () => {
-        try {
-          if (conn.state != 'close') return
-          if (conn.user && conn.user.jid)
-            parent.sendMessage(conn.user.jid, `Connection disconnected...`, MessageType.extendedText)
-          let i = global.conns.indexOf(conn)
-          if (i < 0) return
-          delete global.conns[i]
-          global.conns.splice(i, 1)
-        } catch (e) { conn.logger.error(e) }
-      }, 30000)
-    })
-    global.conns.push(conn)
-  } else throw "Can't make bot insidebot!\n\nhttps://wa.me/' + global.conn.user.jid.split`@`[0] + '?text=.jadibot"
+╔╣ *PREMIUM USER*
+║ • Infinity Limit
+║ • Full Akses Private Chat
+╚══╣ *Harga :* Rp.10.000 / bulan
+
+╔╣ *SEWA BOT*
+║ • Dapat Premium
+║ • Bebas Invit ke 1 Grup
+╚══╣ *Harga :* Rp.15.000 / bulan
+
+╔╣ *JASA RUN BOT*
+║ • Nebeng Run SC Via RDP
+║ • SC wajib *plugin*, bukan case
+╚══╣ *Harga :* Rp.20.000 / bulan
+
+╔╣ *JADI BOT*
+║ • Jadi Bot Azami Always ON
+║ • Custom Namabot, Owner, rules, dll.
+║ • Bisa Req Tampilan atau Fitur
+╚══╣ *Harga :* Rp.25.000 / bulan
+
+- Pembayaran via *OVO / Dana / GoPay*
+  *( tidak ada opsi lain )*
+  ke nomor 082337245566
+- Whatsapp Multi Device
+- Run via RDP (Always ON)
+- Request Fitur? *Chat Link Creator di atas.*`
+	//m.reply(txt)
+	conn.relayMessage(m.chat,  {
+		requestPaymentMessage: {
+			currencyCodeIso4217: 'IDR',
+			amount1000: 25000 * 1000,
+			requestFrom: '0@s.whatsapp.net',
+			noteMessage: {
+				extendedTextMessage: {
+					text: txt,
+					contextInfo: {
+						mentionedJid: [m.sender],
+						externalAdReply: {
+							showAdAttribution: true
+						}
+					}
+				}
+			}
+		}
+	}, {})
+	
+	/*const { prepareWAMessageMedia, generateWAMessageFromContent, proto } = require("@whiskeysockets/baileys")
+	let fs = require('fs')
+	var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/anime.jpg') }, { upload: conn.waUploadToServer })
+	var catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+		"productMessage": {
+			"product": {
+				"productImage": messa.imageMessage,
+				"productId": "5838766206142201",
+				"title": `Sewa Bot`,
+				"description": `gaktau`,
+				"currencyCode": "IDR",
+				"bodyText": `gaktaukalo`,
+				"footerText": `koncol`,
+				"priceAmount1000": "15000000",
+				"productImageCount": 100,
+				"firstImageId": 1,
+				"salePriceAmount1000": "15000000",
+				"retailerId": `ꪶ𝐖𝐫𝐚𝐧𝐳𝐓𝐚𝐦𝐩𝐚𝐧𝐳⿻ꫂ`,
+				"url": "wa.me/6282337245566"
+			},
+			"businessOwnerJid": "6282337245566@s.whatsapp.net",
+		}
+	}), { userJid: m.chat, quoted: m })
+	conn.relayMessage(m.chat, catalog.message, { messageId: catalog.key.id })*/
 }
-handler.help = ['jadibot']
-handler.tags = ['jadibot']
 
-handler.command = /^jadibot$/i
+handler.menugroup = ['jadibot']
+handler.tagsgroup = ['group']
+handler.command = /^(jadibot)$/i
 
-handler.limit = true
-
-module.exports = handler
+export default handler
