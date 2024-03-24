8408487e9e294338b41f297cb858a501d07350bc
AOMAR-7 <163443219+AOMAR-7@users.noreply.github.com>
2024-03-24 10:25:34 +0000

    Update animes-couple.js

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/ovmer_1', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
   await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/ovmar_1', m)
 }
