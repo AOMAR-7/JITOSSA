cae7f12aa162d1393258553189b4e95ed51c50e8
AOMAR-7 <163443219+AOMAR-7@users.noreply.github.com>
2024-03-24 09:42:43 +0000

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -4,14 +4,14 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `*الإسم:* " + name + "\n" + "*Lastup:* " + lastup + "\n" + "*الحجم:* " + size + "\n\n *تابع صانع البوت فى إنستجرام ❤️* \n https://www.instagram.com/ovmar_1`
+    let response = ``*اسم التطبيق:* ${data5.name}\n🕒 *تحذيث أخر : ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\nتابع صاحب البوت لكي تتمكن من الحصول على جديد البوتات \nninstagram.com/ovmar_1``
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ 😁 ]الملف كبير جدًا لذا لن يتم إرساله.'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*الملف كبير جدًا لذا لن يتم إرساله.'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*[😒] خطأ، لم يتم العثور على نتائج لبحثك.*`;
+    throw `*خطأ، لم يتم العثور على نتائج لبحثك.*`;
   }    
 };
 handler.help = ["apk"]
