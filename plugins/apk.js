191323de9614ca8a2ba465547a318dcaf27e9225
AOMAR-7 <163443219+AOMAR-7@users.noreply.github.com>
2024-03-24 09:54:18 +0000

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,10 +1,10 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw ` الأمر المفضل للجميع أمر التطبيقات قم بالاإرسال هاكذا \n*.apk facebbok lite*`;
+ if (!text) throw `*هاذا الأمر المعروف خاص بتحميل التطبيقات قم بالإرسال هاكذا*\n*.apk facebbok lite*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `*اسم التطبيق:* ${data5.name}\n 🕒 *تحذيث أخر:* ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\nما الذي يجعلك لا تتابع  صاحب البوت يا عزيزي  😄نورالدين يحب من يستعمل بوتاته لذا تابعه في حساباته \ninstagram.com/noureddine_ouafy`
+    let response = `*اسم التطبيق:* ${data5.name}\n  *أخر تحديث::* ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\nقم بمتابعة صاحب البوت لكي تتمكن من الإستفادة من جميع البوتات والمزاية 🫵🏻 \ninstagram.com/ovmar_1`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, {text: '*[ 😁 ]الملف كبير جدًا لذا لن يتم إرساله.'}, {quoted: m});
